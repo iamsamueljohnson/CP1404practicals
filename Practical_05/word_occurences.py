0065e5253917845ee1b0d16618ee0b5dc133e248
jc527088 <samsan9237@gmail.com>
2019-04-12 10:24:51 +1000

    Fix word_occurences.py

--- Practical_05/word_occurences.py
+++ Practical_05/word_occurences.py
@@ -1,4 +1,5 @@
-text = input("Enter a random sentence: ")
+# text = input("Enter a random sentence: ")
+text = "this is a collection of words of nice words this is a fun thing it is"
 word_to_count = {}
 words = text.split()
 for word in words:
@@ -6,7 +7,9 @@ for word in words:
         word_to_count[word] += 1
     else:
         word_to_count[word] = 1
-    words = list(word_to_count.keys())
-    words.sort()
-    max_length = max(len(word) for word in words)
+
+words = list(word_to_count.keys())
+words.sort()
+max_length = max(len(word) for word in words)
+for word in words:
     print("{:{}} = {}".format(word, max_length, word_to_count[word]))
