a2f8d5a034c0c3470fd9398ac7c2cf92c998a320
Sam <samsan9237@gmail.com>
2019-05-02 17:45:01 +1000

    Add menu Fix fuel < distance exception

--- Practical_06/car_simulator.py
+++ Practical_06/car_simulator.py
@@ -3,17 +3,36 @@ from Practical_06.car import Car
 
 def main():
     car = []
-
+    minimum_length = 1
     print("Let's drive!")
     name = input("Enter your car name: ")
+    while len(name) < minimum_length:
+        print("Must have a name!")
+        name = input("Enter your car name: ")
     my_car = Car(name, 100)
     car.append(my_car)
     print("{self.name} fuel={self.fuel}, odo={self.odometer}".format(self=my_car))
+    menu = "Menu:\nd) Drive\nr) Refuel\nq) Quit"
+    print(menu)
+    choice = input("Enter your choice: ").upper()
+    while choice != "Q":
+        if choice == "D":
+            distance = int(input("How many km do you wish to drive?"))
+            my_car.drive(distance)
+            print("The car drove {}km.".format(distance))
+            print("{self.name}, fuel={self.fuel}, odo={self.odometer}".format(self=my_car))
 
-    distance = int(input("How many km do you wish to drive?"))
-    my_car.drive(distance)
-    print("The car drove {}km.".format(distance))
-    print("{self.name}, fuel={self.fuel}, odo={self.odometer}".format(self=my_car))
+            if my_car.fuel < distance:
+                print("The car drove {self.fuel} and ran out of fuel.".format(self=my_car))
+
+            print(menu)
+            choice = input("Enter your choice: ").upper()
+
+        else:
+            print("Invalid choice")
+            print(menu)
+            choice = input("Enter your choice: ").upper()
+    print("Thank you")
 
 
 main()
